b687538f7598513b47cd01f6c8f16fecfab1a0f1
Conner Hibbert <hconnerj@byu.edu>
2026-09-22 10:07:48 -0600

    Cleaned up RulesEngine. There's now two private methods getKingPosition which returns the position of the king (important when checking for check) and getAllMoves which iterates through the full chessboard and creates a collection of all the moves one team can make, which is useful for checking for check and checkmate. These functions are used in confirmCheck and confirmCheckmate. Forgot to mention, in previous commit added a newe constructor for the Chessboard which takes another Chessboard as an argument and makes itself a copy of it. Useful for making deep copies of the chessboard.

--- shared/src/main/java/chess/RulesEngine.java
+++ shared/src/main/java/chess/RulesEngine.java
@@ -8,22 +8,10 @@ public class RulesEngine {
 
     public static boolean confirmCheck(ChessBoard board, ChessGame.TeamColor color) {
         ChessPiece piece;
-        ChessPosition kingPosition = null;
-        List<ChessMove> possibleMoves = new ArrayList<>();
-        for (int i = 1; i <= 8; i++) {
-            for (int g = 1; g <= 8; g++) {
-                piece = board.getPiece(new ChessPosition(i, g));
-                if (piece != null) {
-                    if (piece.getTeamColor() != color) {
-                        possibleMoves.addAll(piece.pieceMoves(board, new ChessPosition(i, g)));
-                    } else {
-                        if (piece.getPieceType() == ChessPiece.PieceType.KING) {
-                            kingPosition = new ChessPosition(i, g);
-                        }
-                    }
-                }
-            }
-        }
+        ChessPosition kingPosition = getKingPosition(board, color);
+        ChessGame.TeamColor opColor = ChessGame.TeamColor.WHITE;
+        if (color == ChessGame.TeamColor.WHITE) opColor = ChessGame.TeamColor.BLACK;
+        Collection<ChessMove> possibleMoves = getAllMoves(board, opColor);
         //System.out.println(board.toString());
         for (ChessMove move : possibleMoves) {
             if (move.getEndPosition().equals(kingPosition)) {
@@ -36,19 +24,18 @@ public class RulesEngine {
     }
 
     public static boolean confirmCheckmate(ChessBoard board, ChessGame.TeamColor color) {
-        ChessPosition kingPosition = getKingPosition(board, color);
-        Collection<ChessMove> movesToCheck = board.getPiece(kingPosition).pieceMoves(board, kingPosition);
+        Collection<ChessMove> movesToCheck = getAllMoves(board, color);
         for (ChessMove move : movesToCheck) {
             ChessBoard tempBoard = new ChessBoard(board);
             ChessPiece piece = tempBoard.getPiece(move.getStartPosition());
             tempBoard.addPiece(move.getEndPosition(), piece);
             tempBoard.addPiece(move.getStartPosition(), null);
             if (!confirmCheck(tempBoard, color)) {
-                System.out.println(board.toString() + "Checkmate!");
+                System.out.println(board.toString() + " Not in checkmate");
                 return false;
             }
         }
-        System.out.println(board.toString() + "Not in Checkmate");
+        System.out.println(board.toString() + "Checkmate!");
         return true;
     }
 
@@ -65,5 +52,21 @@ public class RulesEngine {
             }
         }
         return null;
+    }
+
+    private static Collection<ChessMove> getAllMoves(ChessBoard board, ChessGame.TeamColor color) {
+        ChessPiece piece = null;
+        List<ChessMove> possibleMoves = new ArrayList<>();
+        for (int i = 1; i <= 8; i++) {
+            for (int g = 1; g <= 8; g++) {
+                piece = board.getPiece(new ChessPosition(i, g));
+                if (piece != null) {
+                    if (piece.getTeamColor() == color) {
+                        possibleMoves.addAll(piece.pieceMoves(board, new ChessPosition(i, g)));
+                    }
+                }
+            }
+        }
+        return possibleMoves;
     }
 }
